15e199bbfe37a568ae9ae865ec30e0a83cd902f3
kimch-man <kimch3141592@gmail.com>
2024-12-05 17:50:10 +0900

    csv & json sort

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,17 @@ import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
+  const res_gapyeong = await fetch('https://raw.githubusercontent.com/kimch-man/yeosuerp/refs/heads/main/gapyeong.csv')
+  // const data_gapyeong = await res_gapyeong.json();
+
+  console.log(res_gapyeong)
+
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        {/* {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
       </main>
     </>
   );
